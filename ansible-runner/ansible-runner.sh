6c59a3a21c294a197395cf200e58596309cac858
Olaf Lipinski <olipinski@superis.eu>
2026-04-22 23:01:14 +0200

    Minor cleanups

--- ansible-runner/ansible-runner.sh
+++ ansible-runner/ansible-runner.sh
@@ -3,8 +3,7 @@
 # Execute ansible-runner command using bash shell with logging option
 # runner user profile is loaded
 
-CMD="docker exec -it ansible-runner \
-     /bin/bash -lic"
+ CMD="docker exec -it ansible-runner /bin/bash -lic"
 
 # Execute docker run command
 $CMD "$(printf ' %q' "$@")"
